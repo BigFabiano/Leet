ebf1391f903e3b801eee1a8211578383eb9c1342
Diego Neri <diego@diegoneri.com>
2021-03-15 07:53:57 -0300

    Adiciona replace para L/l para 1

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace Leet
             Console.ForegroundColor = ConsoleColor.Green;    
             string textoDigitado = "Hackers usavam leet antes de virar modinha";
             Console.WriteLine(textoDigitado);
-            string recebetexto = textoDigitado.Replace("a","4").Replace("e","3").Replace("i","1").Replace("o","0").Replace("t","7").Replace("s","5").Replace("A","4").Replace("E","3").Replace("I","1").Replace("O","0").Replace("T","7").Replace("S","5");
+            string recebetexto = textoDigitado.Replace("a","4").Replace("e","3").Replace("i","1").Replace("l","1").Replace("o","0").Replace("t","7").Replace("s","5").Replace("A","4").Replace("E","3").Replace("I","1").Replace("L","1").Replace("O","0").Replace("T","7").Replace("S","5");
             Console.WriteLine(recebetexto);
             Console.ResetColor();
         }
